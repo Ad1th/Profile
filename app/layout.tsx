3840c4398d3ca3d4842cb74dcdd1ef56ebcd38ec
Ad1th <adith2505@outlook.com>
2025-04-06 13:55:21 +0530

    Fix hero and title

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,29 +1,33 @@
-import type React from "react"
-import "@/app/globals.css"
-import { ThemeProvider } from "@/components/theme-provider"
+import type React from "react";
+import "@/app/globals.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 export const metadata = {
-  title: "John Doe - Full Stack Developer",
-  description: "Personal portfolio website of John Doe, a Full Stack Developer and UI/UX Designer",
-    generator: 'v0.dev'
-}
+  title: "Adith Manikonda",
+  description:
+    "Personal portfolio website of Adith Manikonda, an Engineering Freshman at VIT",
+  generator: "v0.dev",
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
           {children}
         </ThemeProvider>
       </body>
     </html>
-  )
+  );
 }
 
-
-
-import './globals.css'
+import "./globals.css";

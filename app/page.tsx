47b1fcaa149d62c66afd513a64ad79104ca70c91
Ad1th <90973387+Ad1th@users.noreply.github.com>
2026-05-27 23:23:01 +0530

    add hackathons to desktop view

--- app/page.tsx
+++ app/page.tsx
@@ -62,6 +62,7 @@ export default function Page() {
         <>
           <DesktopCinematicTransition />
           <Patents />
+          <Hackathons />
           <Achievements />
           <Timeline />
           <Contact />
